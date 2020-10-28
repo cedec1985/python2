bec7ac7e05d8ff02914acf32991d7dbd0c51cb6b
cedec1985 <69531112+cedec1985@users.noreply.github.com>
2020-10-28 15:43:52 +0100

    Add files via upload

---
+++ langage C.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    std::cout <<"hello world" << std::endl;
+    std::cout <<"l love programming\n" << "i love C++"<< std::endl;
+int num=30;
+cin >> num;
+int a,b=10;
+std::cout << "enter a number\n"<<std::endl;
+cin>> a;
+    std::cout <<"enter another number \n"<<std::endl;
+cin >>b;
+int sum=a+b;
+    std::cout<<"la somme est égale à :"<<sum<<std::endl;
+int mark = 50;
+if (mark<50){
+    std::cout<<"you failed"<<std::endl;}
+else{
+    std::cout<<"you passed"<<std::endl;}
+
+int marking = 100;
+if(marking<50){
+    std::cout<<"you failed"<<std::endl;}
+    if (marking ==100){
+        std::cout<<"perfect"<<std::endl;
+    }
+return 0;
+}
